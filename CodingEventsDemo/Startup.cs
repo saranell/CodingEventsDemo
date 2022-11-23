b2f4587d1c96531eab63210442f499056451f8d1
saranell <saranell92@gmail.com>
2022-11-23 15:11:15 -0600

    fixing startup.cs

--- CodingEventsDemo/Startup.cs
+++ CodingEventsDemo/Startup.cs
@@ -27,7 +27,7 @@ namespace CodingEventsDemo
         {
             services.AddControllersWithViews();
             services.AddDbContext<EventDbContext>(options =>
-                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version())));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
